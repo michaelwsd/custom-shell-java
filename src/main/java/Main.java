8bc0bf6da530284ef8b39a291bf6c52881edb1d4
Michael Wang <a0416517630@icloud.com>
2025-12-03 17:18:11 +1100

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,15 @@
+import java.io.File;
+import java.nio.file.Files;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+    public static Set<String> commands = Set.of("exit", "echo", "type"); // immutable set 
+    public static String[] dirs = System.getenv("PATH").split(File.pathSeparator);
+
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        Set<String> commands = new HashSet<>(Set.of("exit", "echo", "type"));
 
         while (true) {
             System.out.print("$ ");
@@ -17,16 +21,32 @@ public class Main {
                 System.out.println(line.substring(5));
             } else if (line.startsWith("type ")) {
                 String command = line.substring(5);
-                if (commands.contains(command)) {
-                    System.out.println(command + " is a shell builtin");
-                } else {
-                    System.out.println(command + ": not found");    
-                }
+                runType(command);  
+                
             } else {
                 System.out.println(line + ": command not found");
             }
         }
 
         scanner.close();
+    }
+
+    public static void runType(String cmd) {
+        if (commands.contains(cmd)) {
+            System.out.println(cmd + " is a shell builtin");
+            return;
+        } 
+
+        // search for files
+        for (String dir: dirs) {
+            File f = new File(dir, cmd); // checks if file exists in this directory
+
+            if (f.exists() && f.canExecute()) {
+                System.out.println(cmd + " is " + f.getAbsolutePath());
+                return;
+            }
+        }
+
+        System.out.println(cmd + ": not found");  
     }
 }
