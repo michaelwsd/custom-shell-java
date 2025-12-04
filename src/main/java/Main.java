f13180a400eb2c247d88cebc963c8c468d0e568d
Michael Wang <a0416517630@icloud.com>
2025-12-04 21:14:47 +1100

    added uparrow nav

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,33 @@
-import java.util.Scanner;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.UserInterruptException;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+import org.jline.reader.EndOfFileException;
 
 public class Main {
-    public static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) throws Exception {
 
-        while (true) {
-            System.out.print("$ ");
-            String line = scanner.nextLine().strip();
+        try (Terminal terminal = TerminalBuilder.terminal()) {
+            LineReader reader =
+                    LineReaderBuilder.builder()
+                            .terminal(terminal)
+                            .option(LineReader.Option.DISABLE_EVENT_EXPANSION, true)
+                            .build();
 
-            if (line.isEmpty()) continue;
+            while (true) {
+                try {
+                    String line = reader.readLine("$ ");
+                    if (line.isEmpty()) continue;
 
-            if (!Builtins.runCommand(line)) {
-                if (!Executor.runProgram(line)) {
-                    System.out.println(line + ": command not found");
+                    if (!Builtins.runCommand(line)) {
+                        if (!Executor.runProgram(line)) {
+                            System.out.println(line + ": command not found");
+                        }
+                    }
+
+                } catch (UserInterruptException | EndOfFileException e) {
+                    break;
                 }
             }
         }
